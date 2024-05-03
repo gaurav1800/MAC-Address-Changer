5227ef66b4836c918e79fc2270dacb6b42d8a37f
Gaurav <gauravsanghu@yahoo.com>
2024-05-02 23:52:26 -0400

    Final commit

--- mac_changer.py
+++ mac_changer.py
@@ -9,6 +9,8 @@ import optparse
 # allows the use of regular expressions
 import re
 
+
+
 def get_arguments():
     # Create an instance that can handle user input using arguments
     parser = optparse.OptionParser()
@@ -21,25 +23,38 @@ def get_arguments():
         parser.error("[-] Please specify a new MAC address, use --help for more info.")
     return argument_values
 
-
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for: " + interface + " into " + new_mac)
 
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
+    
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(rb"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0).decode("utf-8")
+    else:
+        print("[-] Could not read the MAC address.")
 
 
 argument_values = get_arguments()
-# change_mac(argument_values.interface, argument_values.new_mac)
 
-ifconfig_result = subprocess.check_output(["ifconfig", argument_values.interface])
-print(ifconfig_result)
+current_mac = get_current_mac(argument_values.interface)
+print("Current MAC address = " + str(current_mac))
 
-mac_address_search_result = re.search(rb"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-print(mac_address_search_result.group(0))
+change_mac(argument_values.interface, argument_values.new_mac)
+
+current_mac = get_current_mac(argument_values.interface)
+
+if current_mac == argument_values.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
+elif current_mac != argument_values.new_mac:
+    print("[-] MAC address did not get changed.")
 
 
-# To run in terminal, execute the following:
+# To run in terminal, execute either one of the following:
 # python3 mac_changer.py --interface <interface> --mac <new MAC address>
+# python3 mac_changer.py -i <interface> -m <new MAC address>
 
